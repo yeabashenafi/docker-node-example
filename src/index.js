46d509ae4e374d45b18229489dc27f25a7c4e08b
Yeabsira Ashenafi <ashenafiyeabsira@gmail.com>
2021-12-31 15:40:32 +0300

    setup a simple express project using docker

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const app = require('express')();
+
+app.get('/',(req,res) => 
+    res.json({
+        message: 'Docker-node is easy'
+    })
+);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`app listening on http://localhost:${port}`)); 
